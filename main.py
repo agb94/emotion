062b77c033a442e82feaaf6a3a3d46c1bdbd4c37
Gabin An <agb94@kaist.ac.kr>
2017-09-23 21:55:24 +0900

    crop faces

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2
 import sys
 
+crop_dir = "./crop/"
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("[Usage] python main.py [video_file_name] [casc_path]")
@@ -12,28 +14,33 @@ if __name__ == "__main__":
 
     # Create the haar cascade
     faceCascade = cv2.CascadeClassifier(cascPath)
-
+    
+    speed = 10
+    counter = 0
     while(cap.isOpened()):
         ret, frame = cap.read()
         cv2.imshow('frame', frame)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
-        # Detect faces in the image
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.3,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags = cv2.CASCADE_SCALE_IMAGE
-        )
-        
-        # Draw a rectangle around the faces
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        
+        if counter % speed == 0:
+            # Detect faces in the image
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.3,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags = cv2.CASCADE_SCALE_IMAGE
+            )
+            
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in faces:
+                crop_img = frame[y:(y+h), x:(x+w)]
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                cv2.imwrite(crop_dir + "{}-x{}y{}w{}h{}.jpg".format(counter, x, y, w, h), crop_img)
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        counter += 1
 
     cap.release()
     cv2.destroyAllWindows()
