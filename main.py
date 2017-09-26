5980b27142230678616b2896d798ee8dcc7666ab
Gabin An <agb94@kaist.ac.kr>
2017-09-26 16:30:18 +0900

    Generate metadata file for video

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import numpy as np
 import cv2
 import sys
+import os
 
-crop_dir = "./crop/"
+CROP_DIR = "./crop/"
+INTERVAL = 10
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -11,19 +13,24 @@ if __name__ == "__main__":
     
     cap = cv2.VideoCapture(sys.argv[1])
     cascPath = sys.argv[2]
+    
+    video_name = os.path.splitext(os.path.basename(sys.argv[1]))[0]
+    metadata_file_path = os.path.join(os.path.dirname(sys.argv[1]), video_name) + '.tsv'
+    metadata_file = open(metadata_file_path, 'w')
+    metadata_columns = ['Image File Path', 'MSEC', 'Frame Number', 'Position', 'Character ID'] 
+    metadata_file.write("\t".join(metadata_columns) + "\n")
 
     # Create the haar cascade
     faceCascade = cv2.CascadeClassifier(cascPath)
     
-    speed = 10
-    counter = 0
+    frame_counter = 0
     while(cap.isOpened()):
         ret, frame = cap.read()
         msec = cap.get(cv2.CAP_PROP_POS_MSEC)
         cv2.imshow('frame', frame)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
-        if counter % speed == 0:
+        if frame_counter % INTERVAL == 0:
             # Detect faces in the image
             faces = faceCascade.detectMultiScale(
                 gray,
@@ -34,14 +41,20 @@ if __name__ == "__main__":
             )
             
             # Draw a rectangle around the faces
+            face_counter = 1
             for (x, y, w, h) in faces:
                 crop_img = frame[y:(y+h), x:(x+w)]
-                cv2.imwrite(crop_dir + "{}-{}-x{}y{}w{}h{}.jpg".format(msec, counter, x, y, w, h), crop_img)
+                crop_img_path = CROP_DIR + "{}-{}.jpg".format(frame_counter, face_counter)
+                cv2.imwrite(crop_img_path, crop_img)
+                metadata_file.write("{}\t{}\t{}\t{}\t{}\n".format(crop_img_path, msec, frame_counter, str((x,y,w,h)), -1))
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                face_counter += 1
+
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        counter += 1
+        frame_counter += 1
 
+    metadata_file.close()
     cap.release()
     cv2.destroyAllWindows()
