6d479a42eb4023beec7053455e39ddccc9164267
Gabin An <agb94@kaist.ac.kr>
2017-11-14 13:27:56 +0900

    emotional change analyzer

--- emotional_change_analyzer.py
+++ emotional_change_analyzer.py
@@ -7,35 +7,41 @@ KEYS = [
     '3e89ec1763ba43eabe03342f8b2e8710'
 ]
 headers = {
-    'Content-Type': 'application/octet-stream'
+    'Content-Type': 'application/octet-stream',
+    'Ocp-Apim-Subscription-Key': '6f2f9a102d684ba5a7d846fe3059e2da'
 }
 conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
 
 def get_emotion(image_file_path):
     try:
         with open(image_file_path, 'rb') as f:
-            params = urllib.parse.urlencode({ 'data': f })
-        conn.request("POST", "/emotion/v1.0/recognize?%s" % params, "{body}", headers)
+            data = f.read()
+        conn.request("POST", "/emotion/v1.0/recognize", data, headers)
         response = conn.getresponse()
         data = response.read()
-        return data
         conn.close()
+        return data
     except Exception as e:
         print("[Errno {0}] {1}".format(e.errno, e.strerror))
         return None
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        sys.exit()
-    char_id = int(sys.argv[2])
-    metadata = util.parse_metadata_file(sys.argv[1])
-    metadata = list(filter(lambda r: r['character_id'] == char_id, metadata))
+def characters_emotion(metadata_path, character_id):
+    emotions = []
+    metadata = util.parse_metadata_file(metadata_path)
+    metadata = list(filter(lambda r: r['character_id'] == character_id, metadata))
     metadata = sorted(metadata, key=lambda r: r['frame_number'])
     interval = INTERVAL / len(KEYS)
     for i, row in enumerate(metadata):
         img_path = row['image_file_path']
         key = KEYS[i % len(KEYS)]
         headers['Ocp-Apim-Subscription-Key'] = key
-        print(img_path)
-        print(get_emotion(img_path))
+        emotion = get_emotion(img_path)
+        if emotion:
+            emotions.append((row['frame_number'], emotion))
         time.sleep(interval)
+    return emotions
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        sys.exit()
+    characters_emotion(sys.argv[1], int(sys.argv[2]))
